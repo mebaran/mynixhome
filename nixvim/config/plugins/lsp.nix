af59f3eed82e26fe0d1a5f250a163345cd3a4a64
Mark Baran <mebaran@gmail.com>
2024-12-24 16:47:49 -0800

    Fixed lspsaga typo

--- nixvim/config/plugins/lsp.nix
+++ nixvim/config/plugins/lsp.nix
@@ -32,7 +32,7 @@
                 map("n", "<leader>wl", function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, opts_desc("Print workspace folders list"))
-                map("n", "<leader>gd", ${lspsaga "peek_defition"}, opts_desc("Peek definition"))
+                map("n", "<leader>gd", ${lspsaga "peek_definition"}, opts_desc("Peek definition"))
                 map("n", "<leader>gy", ${lspsaga "peek_type_definition"}, opts_desc("Peek t(y)pe definition"))
                 map("n", "<leader>cr", vim.lsp.buf.rename, opts_desc("Code rename"))
                 map("n", "<leader>ca", ${lspsaga "code_action"}, opts_desc("Code action"))
