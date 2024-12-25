f78c64c4498489d7c69aee3f22654caa42e5aa57
Mark Baran <mebaran@gmail.com>
2024-12-24 17:25:21 -0800

    Enabled tab to work like in shell

--- nixvim/config/plugins/autocomplete.nix
+++ nixvim/config/plugins/autocomplete.nix
@@ -9,9 +9,27 @@
           use_nvim_cmp_as_default = true;
         };
         list.selection = "auto_insert";
-        keymap = {
-          preset = "enter";
-        };
+        keymap.__raw = ''
+          {
+            preset = 'enter',
+            ['<Tab>'] = {
+              function(cmp)
+                if cmp.snippet_active() then return
+                else return cmp.select_next() end
+              end,
+              'snippet_forward',
+              'fallback'
+            },
+            ['<S-Tab>'] = {
+              function(cmp)
+                if cmp.snippet_active() then return
+                else return cmp.select_prev() end
+              end,
+              'snippet_prev',
+              'fallback'
+            },
+          }
+        ''; 
       };
     };
   };
