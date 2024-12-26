93d6439a6bb28ae4bcaf0f23087935e25cb034b5
Mark Baran <mebaran@gmail.com>
2024-12-26 15:57:20 -0800

    Continuing to simplify html css autocompleter

--- nixvim/lang/web.nix
+++ nixvim/lang/web.nix
@@ -3,8 +3,6 @@
     lsp.enable = true;
     lsp.servers = {
       emmet_language_server.enable = true;
-      htmx.enable = true;
-      html.enable = true;
       ts_ls.enable = true;
     };
     conform-nvim.settings.formatters_by_ft = {
