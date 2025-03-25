b4409202e083182eff750593f70fe0f4b42e1093
Mark Baran <mebaran@gmail.com>
2025-03-24 23:19:30 -0700

    Added wormhole-rs utility for secure file transfer

--- packages.nix
+++ packages.nix
@@ -25,6 +25,7 @@
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc # it is a calculator for the IPv4/v6 addresses
+    wormhole-rs #magic wormhole with Rust goodness
 
     #sql
     pspg
